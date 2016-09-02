4ca0d74fbc9a20e83d71320c25f8b85076009744
zhouwenwen <zhouwenwen@meizu.com>
2016-09-02 14:29:07 +0800

    commit  MyClass

---
+++ helper/Url.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 简述
+ *
+ * User: zhouwenwen
+ * Date: 2016/9/2
+ * Time: 14:26
+ */
+class Url {
+    public function __construct() {
+        echo "url";
+    }
+}
---
+++ lib/Test.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 简述
+ *
+ * User: zhouwenwen
+ * Date: 2016/9/2
+ * Time: 14:25
+ */
+class Test {
+    public function __construct() {
+        echo "libtest";
+    }
+}

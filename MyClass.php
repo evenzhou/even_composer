1349b1f8c59bcfebc5aba471d8728a11c2d7e684
zhouwenwen <zhouwenwen@meizu.com>
2016-09-02 10:44:06 +0800

    commit  MyClass

---
+++ MyClass.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * 简述
+ *
+ * User: zhouwenwen
+ * Date: 2016/9/2
+ * Time: 10:42
+ */
+class MyClass {
+    public function fooBar() {
+        echo "this is my first project";
+    }
+}
